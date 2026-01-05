a6622d1bc06e6e5b14f96f04f4757088d3669b4b
cyalchvadym-collab <c.yalch.vadym@student.uzhnu.edu.ua>
2026-01-05 17:55:51 +0200

    Add Account class with properties and constructors

--- lab5-main/Account.cs
+++ lab5-main/Account.cs
@@ -1,1 +1,17 @@
+namespace NewspaperKioskLab5
+{
+    class Account
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public Account() { }
+
+        public Account(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+}
 

cad32f76637f772fc33b14f394934420a444ce86
cyalchvadym-collab <c.yalch.vadym@student.uzhnu.edu.ua>
2026-01-05 17:54:39 +0200

    Update Publication.cs

--- lab5-main/Publication.cs
+++ lab5-main/Publication.cs
@@ -1,1 +1,17 @@
+namespace NewspaperKioskLab5
+{
+    class Publication
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Publication() { }
+
+        public Publication(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
 

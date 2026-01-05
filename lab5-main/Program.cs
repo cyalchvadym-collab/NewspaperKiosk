59dc573729d1a1c3659fb7a9223c0de11401ab23
cyalchvadym-collab <c.yalch.vadym@student.uzhnu.edu.ua>
2026-01-05 17:12:31 +0200

    Add files via upload

---
+++ lab5-main/Program.cs
@@ -0,0 +1,12 @@
+using System.Text;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Кіоск газет і журналів працює!");
+    }
+}
---
+++ lab5-main/Role.cs
@@ -0,0 +1,8 @@
+namespace ConsoleApp1;
+
+public enum Role
+{
+    None = 0,
+    Admin = 1,
+    Customer = 2
+}

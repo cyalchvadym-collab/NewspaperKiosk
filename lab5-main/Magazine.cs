c977a4284cefa9f20cb3de9c575afd52456f53af
cyalchvadym-collab <c.yalch.vadym@student.uzhnu.edu.ua>
2026-01-05 17:29:25 +0200

    Delete lab5-main directory

--- lab5-main/Magazine.cs
+++
@@ -1,8 +0,0 @@
-namespace ConsoleApp1;
-
-public class Magazine : Publication
-{
-    public int IssueNumber { get; set; }
-    public string Category { get; set; } = "";
-    public override string TypeName => "Журнал";
-}
